6bc0da22c10d98039cf0d15020baed1434290653
temit <temitoyosi@gmail.com>
2021-05-17 22:58:31 +0100

    first commit

---
+++ API_Versioning/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Versioning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("1.1")]
+    [ApiVersion("2.0")]
+    public class DefaultController : ControllerBase
+    {
+        string[] authors = new string[]
+        {
+            "Yosi Osi","Jaden Smith","Stephen og" 
+        };
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return authors;
+        }
+        [HttpGet("{id}")]
+        [MapToApiVersion("2.0")]
+        public string Get(int id)
+        {
+            return authors[id];
+        }                                                                                                                                                                                                                                                                                                          
+    }
+}
